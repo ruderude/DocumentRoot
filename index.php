0353d4b02300b20a1001f89feb0569eb66b7fa35
ruderude <rude1979@gmail.com>
2021-06-18 02:13:28 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use app\core\Application;
+
+$app = new Application();
+
+$app->router->get('/', function () {
+    return 'Hello, world';
+});
+
+$app->router->get('/contact', function () {
+    return 'Contact';
+});
+
+$app->router->get('/test', function () {
+    return 'Test';
+});
+
+$app->run();
